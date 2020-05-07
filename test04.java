0a3d3d63484cc3eb7fe461682d9a0fbce61ce2e5
malakadasun <greenpagelocal7@gmail.com>
2020-05-07 23:49:18 +0530

    if else java program

---
+++ test04.java
@@ -0,0 +1,12 @@
+public static void main (String[ ] args)
+{
+int Age = 18;
+if (Age <  65)
+      {
+      System.out.println {“You are too young to retire!”);
+      }
+if (Age >= 65)
+      {
+      System.out.println {You are ready for retirement!”);
+      }
+)
